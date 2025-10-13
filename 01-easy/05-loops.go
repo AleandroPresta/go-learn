28688945a4fa444db2644c9bf015dfe0057554b6
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-10-13 12:34:57 +0200

    Implement exercises for traditional and continue loops in Go

--- 01-easy/05-loops.go
+++ 01-easy/05-loops.go
@@ -29,37 +29,16 @@ import "fmt"
 func main() {
 	// Exercise 1: Traditional for loop
 	// TODO: Print numbers from 1 to 10 using a traditional for loop
-	
-	// Exercise 2: While-like loop
-	// TODO: Print numbers from 1 to 5 using for with only a condition
-	
-	// Exercise 3: Infinite loop with break
-	// TODO: Create an infinite loop that prints numbers and breaks when it reaches 3
-	
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("%d\n", i)
+	}
+
 	// Exercise 4: Loop with continue
 	// TODO: Print numbers 1-10 but skip even numbers using continue
-	
-	// Exercise 5: Nested loops
-	// TODO: Create a multiplication table (1-5) using nested for loops
-	
-	// Exercise 6: Range over string
-	// TODO: Iterate over a string and print each character with its index
-	
-	// Exercise 7: Range over slice/array
-	// TODO: Create a slice of your favorite colors and iterate over it
-	//       Print both index and value
-	
-	// Exercise 8: Loop patterns
-	// TODO: Calculate factorial of a number using a for loop
-	// TODO: Calculate the sum of all numbers from 1 to N
-	
-	// Exercise 9: Advanced loop control
-	// TODO: Create nested loops with labels and use labeled break/continue
-	//       Example: Print a pattern but skip certain combinations
-	
-	// Exercise 10: FizzBuzz challenge
-	// TODO: Print numbers 1-50, but:
-	//       - Print "Fizz" for multiples of 3
-	//       - Print "Buzz" for multiples of 5
-	//       - Print "FizzBuzz" for multiples of both 3 and 5
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Printf("%d\n", i)
+	}
 }
